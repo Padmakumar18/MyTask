1020d8169485e7167b1e7f085840d654afcac946
Padma-Kumar-NAF <PadmaKumar.C@nafinc.com>
2026-04-18 21:48:14 +0530

    fix : Sklls updated

---
+++ app/src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'YOUR_SUPABASE_URL',
+  supabaseKey: 'YOUR_PUBLIC_KEY',
+};
---
+++ app/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'YOUR_SUPABASE_URL',
+  supabaseKey: 'YOUR_PUBLIC_KEY',
+};
