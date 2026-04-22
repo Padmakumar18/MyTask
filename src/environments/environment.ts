68d4bd189a33ee3fd35b117d304eee7f4b6b5e9b
Padmakumar18 <padmakumar41759@gmail.com>
2026-04-22 07:05:57 +0530

    Date : 22/4/2026

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  supabaseUrl: 'https://iaouxyrgeajuqwkmkjxh.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlhb3V4eXJnZWFqdXF3a21ranhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0MTczODcsImV4cCI6MjA5MTk5MzM4N30.j6D2txmyn7BZRoc9p3wjdtYrsRqSujEaBmQbAWNlYLM',
-};
